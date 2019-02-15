d7a2d8592e2e97281bbeb195f854674150e8a843
Doble-De <ddelgados96@elpuig.xeill.net>
2019-02-15 18:36:22 +0100

    Empezando

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -1,9 +1,11 @@
 package control;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -13,6 +15,7 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable{
     public String csvFile = null;
     @FXML AnchorPane paneDret;
+    @FXML Button buscar;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
@@ -21,7 +24,25 @@ public class Controller implements Initializable{
 
     @FXML
     public void onClickBtnMenu(ActionEvent actionEvent){
-        String btn = ((JFXButton)actionEvent.getSource()).getID();
+        String btn = ((JFXButton)actionEvent.getSource()).getId();
+
+        if(btn.equals("btnLlista")) {
+            // ListView lsvLlista = new ListView();
+            AnchorPane anchorPaneLlista = null;
+            try {
+
+                anchorPaneLlista = FXMLLoader.load(getClass().getResource("../fxml/llista.fxml"));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            paneDret.getChildren().add(anchorPaneLlista);
+
+        }else {
+            paneDret.getChildren().clear();
+        }
+
+
     }
 
 }
--- src/main/java/control/ControllerLlista.java
+++ src/main/java/control/ControllerLlista.java
@@ -1,13 +1,54 @@
 package control;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import com.opencsv.CSVReader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class ControllerLlista {
+public class ControllerLlista implements Initializable {
+
+    private String csvFile = null;
+    private List<Pacient> p = new ArrayList<>();
+    private ObservableList<Pacient> data;
+
+    @FXML TableView<Pacient> tablePacients;
+    @FXML JFXButton btnLoadFile;
+    @FXML JFXTextField txtDNI, txtNom, txtCognoms;
+    @FXML PieChart idPieChart;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        data = FXCollections.observableArrayList();
+        if (csvFile == null) {
+            btnLoadFile.setText("Click per carregar CSV");
+        } else {
+            setTableView();
+        }
+    }
+
+    private void setTableView(){
+        TableColumn DNI = new TableColumn("DNI");
+        TableColumn Nom = new TableColumn("Nom");
+        TableColumn Cognoms = new TableColumn("Cognoms");
+        TableColumn DataNaix = new TableColumn("Data de Naixament");
+        TableColumn Genre = new TableColumn("Gènere");
+        TableColumn Telefon = new TableColumn("Telèfon");
+        TableColumn pes = new TableColumn("Pes");
+        TableColumn Alçada = new TableColumn("Alçada");
+
+        tablePacients.getColumns().addAll(DNI, Nom, Cognoms, DataNaix, Genre, Telefon, pes, Alçada);
+    }
 }
